51dabc28513e8da6eb1f3bf00bb307908a48f684
arac1123 <neilhouse124@gmail.com>
2024-10-27 04:59:47 +0800

    unfinished: 上傳檔名不能用原檔名(ASCII問題)

--- README.md
+++ README.md
@@ -1,12 +1,25 @@
 # Streaming Platform
+
 ## 介紹
+
 - 一個簡易的串流平台，並且利用 amazon s3 作為資料存儲
 - <a href='https://hackmd.io/8TjXmO6ZQiWGy4wULmGyog'>詳細介紹</a>
+
 ## 架構
+
 - <a href='https://hackmd.io/8r00f8XbQKu0VzChQ0sWsQ?view'>詳細介紹</a>
+
 ## Prerequisite
+
 - s3cmd : `apt install s3cmd`
+
 ## Usage
+
 - `s3cmd --configure`
 - `cd backend && npm install`
 - `cd backend && node index.js`
+
+**windows**
+![alt text](image.png)
+
+- 要將 s3cmd.js 的 script 改成自己的 s3cmd 的位置
--- backend/utilities/s3cmd.js
+++ backend/utilities/s3cmd.js
@@ -1,73 +1,89 @@
 // s3cmd module for js
-
+const { exec } = require("child_process");
 class s3cmd {
-    constructor() {
-		this.bucket_name = "aws-streaming-platform";
-	}
+  constructor() {
+    this.bucket_name = "aws-streaming-platform";
+  }
 
-	// upload file to s3
-	async upload(file) {
-		const result = await this.createProcess(['put', file, `s3://${this.bucket_name}`]);
-		return result;
-	}
+  // upload file to s3
+  async upload(file) {
+    const result = await this.createProcess([
+      "put",
+      file,
+      `s3://${this.bucket_name}`,
+    ]);
+    return result;
+  }
 
-	// list all files in bucket
-	async list() {
-		const result = await this.createProcess(['ls', `s3://${this.bucket_name}`]);
-		return result;
-	}
+  // list all files in bucket
+  async list() {
+    const result = await this.createProcess(["ls", `s3://${this.bucket_name}`]);
+    return result;
+  }
 
-	// get file from s3 by filename
-	async get(file) {
-		const result = await this.createProcess(['get', `s3://${this.bucket_name}/${file}`]);
-		return result;
-	}
+  // get file from s3 by filename
+  async get(file) {
+    const result = await this.createProcess([
+      "get",
+      `s3://${this.bucket_name}/${file}`,
+    ]);
+    return result;
+  }
+  //for linux
+  // getS3Process(params) {
+  // 	const script = "s3cmd";
+  // 	const spawn = require("child_process").spawn;
+  // 	console.log(script, params);
+  // 	return spawn(script, params);
+  // }
 
-	getS3Process(params) {
-		const script = "s3cmd";
-		const spawn = require("child_process").spawn;
-		console.log(script, params);
-		return spawn(script, params);
-	}
+  //for windows
+  getS3Process(params) {
+    const script =
+      "C:/Users/user/AppData/Local/Programs/Python/Python313/Scripts/s3cmd.bat"; // s3cmd
+    const command = `${script} ${params.join(" ")}`;
+    console.log(`Executing command: ${command}`);
 
-	async createProcess(params) {
-		const process = this.getS3Process(params);
-		let result = [];
+    return exec(command);
+  }
 
-        return new Promise((resolve, reject) => { // 包裝成 Promise
+  async createProcess(params) {
+    const process = this.getS3Process(params);
+    let result = [];
 
-            process.stderr.on('data', (data) => {
-                console.error(`stderr: ${data.toString()}`);
-            	resolve({suc : false, msg : data.toString()}); // failed to create container, return the error msg
-            });
+    return new Promise((resolve, reject) => {
+      // 包裝成 Promise
 
-            process.on('exit', (code) => {
-                //console.log(`child process exited with code ${code}`);
-                if (code !== 0) {
-                    reject(new Error(`child process exited with code ${code}`)); // 非 0 退出代碼表示錯誤
-                }
-				else {
-					resolve({suc : true, msg : result});
-				}
-            });
+      process.stderr.on("data", (data) => {
+        console.error(`stderr: ${data.toString()}`);
+        resolve({ suc: false, msg: data.toString() }); // failed to create container, return the error msg
+      });
 
-            process.on('error', (err) => {
-                console.error(err);
-                reject(err); // 子進程啟動失敗
-            });
+      process.on("exit", (code) => {
+        //console.log(`child process exited with code ${code}`);
+        if (code !== 0) {
+          reject(new Error(`child process exited with code ${code}`)); // 非 0 退出代碼表示錯誤
+        } else {
+          resolve({ suc: true, msg: result });
+        }
+      });
 
-            process.stdout.on('data', (data) => {
-		    	try {
-					//resolve({suc : true, msg : data});
-					result.push(data);
-		    	}
-		    	catch(e) {
-					console.error(e);
-                	reject(err); // 子進程啟動失敗
-		    	}
-            });
-		});
-	}
+      process.on("error", (err) => {
+        console.error(err);
+        reject(err); // 子進程啟動失敗
+      });
+
+      process.stdout.on("data", (data) => {
+        try {
+          //resolve({suc : true, msg : data});
+          result.push(data);
+        } catch (e) {
+          console.error(e);
+          reject(err); // 子進程啟動失敗
+        }
+      });
+    });
+  }
 }
 
 module.exports = s3cmd;
